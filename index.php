e8d45bb5b62f68808163c13d33e8962093c17541
Bootcorp <bootcorp@gmail.com>
2022-10-30 07:28:20 +0100

    addGame

--- index.php
+++ index.php
@@ -4,24 +4,33 @@ require 'partials/header.php';
 
 
 require 'partials/nav.php';
-$test = ['test'];
+$test = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6'];
+$game = array_rand($test, $num = 1);
 
 ?>
 <div class="container border h-75">
-    <div class="d-flex align-items-center align-content-center h-100">
-        <div class="col-md-6 ">
-            <div class="text-center">
-                dfsdgsgg
-            </div>
-        </div>
+    <div class="d-flex align-items-center justify-content-center align-content-center h-100">
         <div class="col-md-6">
-            <div class="text-center">
-                gdfgdfgdfg
+            <p id="game"><?= $test[$game] ?></p>
+            <div class="d-flex justify-content-center">
+                <button id="roll" class="roll">roll</button>
+            </div>
+            <div class="">
+                <?php require 'partials/addGame.php' ?>
             </div>
         </div>
     </div>
 </div>
-
+</div>
+<script>
+    const roll = document.getElementById('roll');
+    roll.addEventListener('click', () => {
+        
+        const game = document.getElementById('game');
+        document.game.innerHTML.replkace('htrgd');
+        console.log(game)
+    })
+</script>
 
 <?php
 
---
+++ partials/addGame.php
@@ -0,0 +1,7 @@
+<form action="" method="post">
+    <label for="nom">Nom du jeu</label>
+    <input type="text" name="nom" id="nom">
+    <label for="rules">Règle du jeux</label>
+    <input type="textarea" name="rules" id="rules">
+    <input type="submit" value="valider" id="valider">
+</form>
